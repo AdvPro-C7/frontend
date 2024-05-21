a2f3dd0873706f5a820acd56dfb40495bd1d1a31
venedictchen <venedictchen@gmail.com>
2024-05-21 23:18:38 +0700

    add navbar

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { IoPersonCircleOutline, IoBagHandleOutline } from "react-icons/io5";
+
+interface NavLinkProps {
+    href: string;
+    isActive: boolean;
+    children: React.ReactNode;
+}
+
+function NavLink({ href, isActive, children }: NavLinkProps) {
+    return (
+        <a
+            className={`transition-opacity px-3 py-1 rounded-full ${isActive ? "active underline font-bold bg-red-primary hover:opacity-70" : "hover:text-red-primary"} `}
+            href={href}
+        >
+            {children}
+        </a>
+    );
+}
+
+const Navbar = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        setIsLoaded(true);
+    }, []);
+
+    return (
+        <nav className="fixed z-40 w-full bg-primary shadow-md">
+            <div className="flex justify-between items-center mx-12 py-4 md:mx-24">
+                <div className="flex items-center">
+                    <NavLink href="/" isActive={pathname === "/"}>
+                        <Image
+                            src={'/bookid.png'}
+                            alt="marmut logo"
+                            className="cursor-pointer hover:opacity-100"
+                            width={200}
+                            height={60}
+                        />
+                    </NavLink>
+                </div>
+                <div className="flex gap-4 md:gap-14 text-center items-center">
+                    <IoPersonCircleOutline className="text-blue-100 text-3xl md:text-4xl cursor-pointer hover:text-blue-300" />
+                    <IoBagHandleOutline className="text-blue-100 text-3xl md:text-4xl cursor-pointer hover:text-blue-300" />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
