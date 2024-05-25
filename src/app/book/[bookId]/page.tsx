cdb15468a98af4827b6415f19505ee599df64d76
Athar Adista <athar0165@gmail.com>
2024-05-25 18:21:27 +0700

    Add : Implement FE user Search

--- src/app/book/[bookId]/page.tsx
+++ src/app/book/[bookId]/page.tsx
@@ -6,15 +6,17 @@ import { useParams } from "next/navigation";
 import PopUpFormEditBook from "../../components/PopUpFormUpdateBook";
 import { BookDetailProps, BookDetailPropsForAdd } from '../../types/BookDetailProps';
 import PopUpFormAddBook from '../../components/PopUpFormAddBook';
+import { userContext } from '@/app/contexts/AuthContext';
+import { useRouter } from 'next/navigation';
 
 const DetailsBookPageView: React.FC = () => {
+    const router = useRouter();
     const params = useParams();
     const { bookId } = params;
     const [bookDetails, setBookDetails] = useState<BookDetailProps | null>(null);
-
+    const { state } = userContext();
     const [isPopupOpen, setIsPopupOpen] = useState(false);
     const [isPopupOpenForAdd, setIsPopupOpenForAdd] = useState(false);
-
 
     const getData = async () => {
         try {
@@ -74,6 +76,41 @@ const DetailsBookPageView: React.FC = () => {
         }
     };
 
+    const handleShare = () => {
+        if (navigator.share) {
+            navigator.share({
+                title: bookDetails?.title,
+                text: `Check out this book: ${bookDetails?.title}`,
+                url: window.location.href
+            }).then(() => {
+                console.log('Thanks for sharing!');
+            }).catch(console.error);
+        } else {
+            // Fallback: Copy URL to clipboard or provide other share options
+            navigator.clipboard.writeText(window.location.href)
+                .then(() => {
+                    alert('Link copied to clipboard!');
+                })
+                .catch(console.error);
+        }
+    };
+
+    const handleDeleteBook = async () => {
+
+        if (bookDetails != null && bookDetails.sold > 0) {
+            alert("Buku tidak dapat dihapus karena sudah pernah terjual.");
+        } else {
+            try {
+                const response = await axios.delete(`http://localhost:8081/api/book-details/1`, {
+                });
+                console.log('Book deleted:', response.data);
+                router.push('/book/1'); // Redirect to book list page after deletion
+            } catch (error) {
+                console.error('Error deleting book:', error);
+            }
+        }
+    };
+
     useEffect(() => {
         getData();
     }, [bookId]);
@@ -84,15 +121,15 @@ const DetailsBookPageView: React.FC = () => {
 
     return (
         <div className="flex flex-col min-h-screen pt-28">
-            <div className="px-20 py-12">
-                <div className="flex ml-24 mt-8">
+            <div className="px-20 py-12 mr-24">
+                <div className="flex mr-24 mt-8">
                     {bookDetails ? (
                         <>
-                            <div className="flex flex-col p-3 mr-20 ml-4">
+                            <div className="flex flex-col p-3 mr-20">
                                 <img src={bookDetails.coverPicture} alt={bookDetails.title} width={200} />
                             </div>
                             <div className="flex flex-col text-gray-500">
-                                <h1 className="text-2xl font-bold">{bookDetails.title}</h1>
+                                <p className="text-4xl font-bold">{bookDetails.title}</p>
                                 <p className='pt-2'>{bookDetails.author}</p>
                                 <div className='flex flex-col pt-8 space-y-10'>
                                     <div>
@@ -102,10 +139,22 @@ const DetailsBookPageView: React.FC = () => {
                                             <p className='text-sm'>Sold: {bookDetails.sold}</p>
                                         </div>
                                     </div>
-                                    <div className='flex space-x-5'>
-                                        <button className='btn btn-primary px-10 text-white-100' onClick={handleEditClick}>Edit</button>
-                                        <button className='btn btn-accent px-10 text-white-100' onClick={handleAddClick}>Add</button>
-                                    </div>
+                                    { state.role == 'admin' && (
+                                        <div className='flex space-x-5'>
+                                            <button className='btn btn-primary px-10 text-white-100' onClick={handleEditClick}>Edit</button>
+                                            <button className='btn btn-accent px-10 text-white-100' onClick={handleAddClick}>Add</button>
+                                            <button className='btn btn-secondary px-10 text-white-100' onClick={handleShare}>Share</button>
+                                            <button onClick={handleDeleteBook} className='btn btn-secondary px-10 text-white-100'>Delete Book</button>
+                                        </div>
+                                    )}
+                                    { state.role == 'customer' && (
+                                        <div className='flex space-x-5'>
+                                            <button className='btn btn-primary px-10 text-white-100' onClick={handleEditClick}>Edit</button>
+                                            <button className='btn btn-accent px-10 text-white-100' onClick={handleAddClick}>Add</button>
+                                            <button className='btn btn-secondary px-10 text-white-100' onClick={handleShare}>Share</button>
+                                            <button onClick={handleDeleteBook} className='btn btn-secondary px-10 text-white-100'>Delete</button>
+                                        </div>
+                                    )}
                                 </div>
                                 <div className='flex flex-col mt-10 '>
                                     <p className='font-bold text-gray-600'>Deskripsi Buku</p>
