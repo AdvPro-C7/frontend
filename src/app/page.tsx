0d124c9da47642f8bc31f075f7580b0750274c25
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-05-27 00:15:15 +0700

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState, useEffect, useContext } from "react";
 import Image from "next/image";
+import BookList from "./book-list/page";
+import '../app/auth/auth.css'
 
 interface Book {
   coverImageUrl: string;
@@ -16,48 +18,7 @@ interface Book {
 }
 
 export default function Home() {
-  const [books, setBooks] = useState<Book[]>([]);
-
-  useEffect(() => {
-    fetch("https://admin-hkqa74sxta-ew.a.run.app/api/best-selling-books")
-      .then((response) => response.json())
-      .then((data) => {
-        setBooks(data.slice(0, 10));
-      })
-      .catch((error) => {
-        console.error("Error fetching book data:", error);
-      });
-  }, []);
-
   return (
-    <main className="flex min-h-screen bg-primary flex-col items-center justify-between p-24">
-      <div className="book-list">
-        {/* Map over the books array and render each book */}
-        {books.map((book, index) => (
-          <div key={index} className="book-item">
-            <div className="book-cover">
-              {/* Use the Next.js Image component for optimized image rendering */}
-              <Image
-                src={book.coverImageUrl}
-                alt={book.title}
-                width={200}
-                height={300}
-              />
-            </div>
-            <div className="book-info">
-              <h2 className="book-title">{book.title}</h2>
-              <p className="book-author">{book.author}</p>
-              <p className="book-publisher">{book.publisher}</p>
-              <p className="book-published-date">{book.publishDate}</p>
-              <p className="book-description">{book.description}</p>
-              <p className="book-price">{book.price}</p>
-              <p className="book-stock">Stock: {book.stock}</p>
-              <p className="book-isbn">{book.isbn}</p>
-              <p className="book-category">{book.category}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </main>
+    <BookList/>
   );
 }
