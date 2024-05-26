7109b3bf633271b050ef83700c789c8871c86a3d
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-05-26 12:17:53 +0700

    feat: remove unused function

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -29,7 +29,6 @@ const UserProfile: React.FC = () => {
 
     const [cloudName] = useState("dzjfu0tcd");
     const [uploadPreset] = useState("ml_default");
-    const [uploadedFileUrl, setUploadedFileUrl] = useState('');
 
     const router = useRouter();
 
